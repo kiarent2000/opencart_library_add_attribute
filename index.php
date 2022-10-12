8d4c23d0ccaae07ad310f4d835a23b09e690fc31
Sergiy Soroka <vesper7777777@gmai.com>
2022-10-12 07:18:09 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/class/'.$class.'.php';
+});
+
+include(__DIR__.'/config.php');
+$dbh = DB::getInstance()->connect();
+
+
+
+
+
+
+
+
+?>
